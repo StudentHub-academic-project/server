29ca8b91d1353a078ae10ece724a88aee94e689b
Kiril Yakymchuk <stbestich@gmail.com>
2024-05-10 14:28:17 +0300

    Connect to db

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,7 +1,6 @@
 import path from 'node:path';
 import * as os from 'os';
 import { Sequelize } from 'sequelize';
-import { PostModel, UserModel } from './models';
 
 const dbName = 'studenthub_db';
 const rootDir = path.join(os.homedir(), '.studenthub');
@@ -12,9 +11,3 @@ export const sequelize = new Sequelize({
   storage: dbPath,
   logging: false,
 });
-
-(async () => {
-  await sequelize.authenticate();
-  await UserModel.sync({ force: true });
-  await PostModel.sync({ force: true });
-})();
