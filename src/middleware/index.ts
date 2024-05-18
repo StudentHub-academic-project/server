e8b570e0bffe402d410ac01175446ce4887466d0
Kiril Yakymchuk <stbestich@gmail.com>
2024-05-19 00:17:01 +0300

    login validation

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,3 +1,4 @@
 export * from './rate-limitter';
 export * from './signup-validation';
 export * from './signin-validation';
+export * from './login.validation';
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,8 @@
 import express from "express";
+import {isLoggedIn} from "../middleware";
 
 export const UserController = express.Router();
 
+UserController.use(isLoggedIn);
 UserController.get('/me');
 UserController.patch('/me');
