e7cc56d2c7ec3d0767689ea441062ea10b752b18
Kiril Yakymchuk <stbestich@gmail.com>
2024-05-19 01:01:58 +0300

    refactored get user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,8 +3,26 @@ import {UserModel} from "../db";
 import {EdituserDto} from "./dto";
 import {handleError} from "@stlib/utils";
 
-export const getUser = (req: Request, res: Response) => {
-  return res.status(200).json({ profile: req.user });
+export const getUser = async (req: Request, res: Response) => {
+  try {
+    const { email } = req.user;
+
+    const [user] = await UserModel.findAll({
+      where: {
+        email
+      }
+    })
+
+    if(!user) {
+      return res.status(404).json('Not found.');
+    }
+
+    return res.status(200).json({ user });
+  } catch (error) {
+    await handleError(error, () => {
+      res.status(500).json({ error: 'Internal server error.' });
+    })
+  }
 }
 
 export const editUser = async (req: Request, res: Response) => {
