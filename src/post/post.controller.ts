98ed8bd4ddff0cc6afb183197ac1c6cdcc341873
Kiril Yakymchuk <stbestich@gmail.com>
2024-05-19 18:50:52 +0300

    Create post

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import {isLoggedIn} from "../middleware";
-import {getAllPosts, getPostById} from "./post.service";
+import {createPost, getAllPosts, getPostById} from "./post.service";
 
 export const PostController = express.Router();
 
 PostController.use(isLoggedIn);
 PostController.get('/', getAllPosts);
 PostController.get('/:post_id', getPostById)
-PostController.post('/')
+PostController.post('/', createPost)
 PostController.patch('/:post_id')
 PostController.delete('/:post_id')
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,6 +1,8 @@
 import {PostModel} from "../db";
 import { Request, Response } from "express";
 import {handleError} from "@stlib/utils";
+import {CreatePostDto} from "./dto";
+import { v4 as uuidv4 } from 'uuid';
 
 export const getAllPosts = async (req: Request, res: Response) => {
   try {
@@ -35,6 +37,31 @@ export const getPostById = async (req: Request, res: Response) => {
     }
 
     return res.status(200).json({ post });
+  } catch (error) {
+    await handleError(error, () => {
+      res.status(500).json({ error: 'Internal server error.' });
+    })
+  }
+}
+
+export const createPost = async (req: Request, res: Response) => {
+  try {
+    const { sub } = req.user;
+    const dto: CreatePostDto = req.body;
+
+    if(!sub) {
+      return res.status(403).json({ message: 'Forbidden.' });
+    }
+
+    const post = await PostModel.create({
+      uuid: uuidv4(),
+      userId: sub,
+      title: dto.title,
+      content: dto.content,
+      rating: 0,
+    })
+
+    return res.status(201).json({ post });
   } catch (error) {
     await handleError(error, () => {
       res.status(500).json({ error: 'Internal server error.' });
