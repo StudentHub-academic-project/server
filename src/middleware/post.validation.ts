f5661e165afa3796f4ed09cf71d9ba43e5e0e31d
Kiril Yakymchuk <stbestich@gmail.com>
2024-05-20 13:04:05 +0300

    rating validator

--- src/middleware/post.validation.ts
+++ src/middleware/post.validation.ts
@@ -10,6 +10,10 @@ export const postValidation = [
     .isString()
     .isLength({ min: 25 })
     .withMessage('Content must be 25 or more characters length.'),
+  body('rating')
+    .isInt({ min: 0, max: 5 })
+    .optional()
+    .withMessage('Rating must be specified in range (0; 5)'),
 
   (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
