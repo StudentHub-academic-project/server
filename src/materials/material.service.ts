d49dfd0e526e61ca887a0d1078479202b729b2ce
Kiril Yakymchuk <stbestich@gmail.com>
2024-05-21 15:57:58 +0300

    Fixed materials.service

--- src/materials/material.service.ts
+++ src/materials/material.service.ts
@@ -4,12 +4,12 @@ import {rootDir} from "./multer.config";
 import {handleError, isExists} from "@stlib/utils";
 import * as fs from "fs";
 
-const getFilepath = async (req: Request, res: Response) => {
+const getFilepath = async (req: Request, res: Response): Promise<{ filePath: string, OK: boolean }> => {
   const { sub } = req.user;
   const { filename } = req.params;
 
   if(!sub) {
-    return res.status(403).json({ error: 'Forbidden.' });
+    throw Error('No sub in user.req.');
   }
 
   const filePath = path.join(rootDir, 'storage', sub, filename);
@@ -17,46 +17,72 @@ const getFilepath = async (req: Request, res: Response) => {
   const fileExists = await isExists(filePath);
 
   if(!fileExists) {
-    return res.status(403).json({ error: 'download: No such file or directory.' });
+    return { filePath, OK: false };
   }
 
-  return filePath;
+  return { filePath, OK: true };
 }
 
 export const getAllMaterials = async (req: Request, res: Response) => {
-  const { sub } = req.user;
+  try {
+    const { sub } = req.user;
 
-  if(!sub) {
-    return res.status(403).json({ error: 'Forbidden.' });
+    if(!sub) {
+      return res.status(403).json({ error: 'Forbidden.' });
+    }
+
+    const dirPath = path.join(rootDir, 'storage', sub);
+
+    const dirExists = await isExists(dirPath);
+
+    if(!dirExists) {
+      return res.status(403).json({ error: 'No such file or directory.' });
+    }
+
+    const files = await fs.promises.readdir(dirPath);
+
+    return res.status(200).json({ files });
+  } catch (error) {
+    return await handleError(error, () => {
+      res.status(500).json({ error: 'Internal server error.' });
+    })
   }
-
-  const dirPath = path.join(rootDir, 'storage', sub);
-
-  const dirExists = await isExists(dirPath);
-
-  if(!dirExists) {
-    return res.status(403).json({ error: 'download: No such file or directory.' });
-  }
-
-  const files = await fs.promises.readdir(dirPath);
-
-  return res.status(200).json({ files });
 }
 
 export const downloadFile = async (req: Request, res: Response) => {
-  const filePath = await getFilepath(req, res);
+  try {
+    const { filePath, OK } = await getFilepath(req, res);
 
-  return res.download(filePath.toString(), async (error) => {
-    await handleError(error, () => {
+    if(!OK) {
+      return res.status(404).json({ error: 'No such file or directory.' })
+    }
+
+    return res.download(filePath, async (error) => {
+      await handleError(error, () => {
+        res.status(500).json({ error: 'Internal server error.' });
+      })
+    })
+  } catch (error) {
+    return await handleError(error, () => {
       res.status(500).json({ error: 'Internal server error.' });
     })
-  })
+  }
 }
 
 export const deleteMaterial = async (req: Request, res: Response) => {
-  const filePath = await getFilepath(req, res);
+  try {
+    const { filePath, OK } = await getFilepath(req, res);
 
-  await fs.promises.rm(filePath.toString());
+    if(!OK) {
+      return res.status(404).json({ error: 'No such file or directory.' })
+    }
 
-  return res.status(204).json({ message: 'File has been deleted.' });
+    await fs.promises.rm(filePath);
+
+    return res.status(204).json({ message: 'File has been deleted.' });
+  } catch (error) {
+    return await handleError(error, () => {
+      res.status(500).json({ error: 'Internal server error.' });
+    })
+  }
 }
