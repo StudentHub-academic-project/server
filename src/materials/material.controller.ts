e30f047562225b10f66f56d38e75bac113bdf88f
Kiril Yakymchuk <stbestich@gmail.com>
2024-05-21 15:38:02 +0300

    updated material controller

--- src/materials/material.controller.ts
+++ src/materials/material.controller.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import {isLoggedIn} from "../middleware";
-import {downloadFile} from "./material.service";
+import {deleteMaterial, downloadFile, getAllMaterials} from "./material.service";
 import {upload} from "./multer.config";
 
 export const MaterialController = express.Router();
 
 MaterialController.use(isLoggedIn);
 MaterialController.get('/:filename', downloadFile);
+MaterialController.get('/', getAllMaterials);
+MaterialController.delete('/:filename', deleteMaterial);
 MaterialController.post('/', upload.any(), (_req, res) => {
   return res.status(201).json({ message: 'File has been uploaded.' });
 })
