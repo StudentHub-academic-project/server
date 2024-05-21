d42230505127e724d7923f974039da4401b5b607
Kiril Yakymchuk <stbestich@gmail.com>
2024-05-21 15:13:33 +0300

    Configure multer

---
+++ src/materials/multer.config.ts
@@ -0,0 +1,25 @@
+import * as fs from 'fs';
+import multer from "multer";
+import path from "node:path";
+import os from "os";
+
+const storage = multer.diskStorage({
+  destination: async (req, _file, callback) => {
+    const { sub } = req.user;
+
+    if(!sub) {
+      throw new Error('Empty user.sub in request.');
+    }
+
+    const rootDir = path.join(os.homedir(), '.studenthub');
+    const uploadDir = path.join(rootDir, 'storage', sub);
+    await  fs.promises.mkdir(uploadDir, { recursive: true }).then(() => {
+      callback(null, uploadDir);
+    });
+  },
+  filename: (_req, file, callback) => {
+    callback(null, file.originalname);
+  }
+})
+
+export const upload = multer({ storage });
